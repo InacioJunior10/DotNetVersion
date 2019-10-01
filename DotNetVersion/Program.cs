7aebf1d7cea06e4d33a24330a3387203d3703d75
InacioJunior10 <inaciojunior10@hotmail.com>
2019-10-01 01:18:15 -0300

    Verificar as versões do .NET Framework instaladas no computador.

---
+++ DotNetVersion/Program.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.ObjectModel;
+
+namespace DotNetVersion
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Versão utilizada: ");
+            Get45PlusFromRegistry();
+            Console.WriteLine();
+
+            Console.WriteLine("Outras versões do .Net Framework instaladas: ");
+            foreach (Version ver in InstalledDotNetVersions())
+                Console.WriteLine(ver);
+            Console.WriteLine();
+
+            Console.WriteLine("Histórico de updates: ");
+            GetUpdateHistory();
+            Console.WriteLine();
+                       
+            Console.ReadKey();
+        }
+
+        private static void Get45PlusFromRegistry()
+        {
+            const string subkey = @"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\";
+
+            using (var ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey(subkey))
+            {
+                if (ndpKey != null && ndpKey.GetValue("Release") != null)
+                {
+                    Console.WriteLine($".NET Framework Version: {CheckFor45PlusVersion((int)ndpKey.GetValue("Release"))}");
+                }
+                else
+                {
+                    Console.WriteLine(".NET Framework Version 4.5 or later is not detected.");
+                }
+            }
+
+            // Checking the version using >= enables forward compatibility.
+            string CheckFor45PlusVersion(int releaseKey)
+            {
+                if (releaseKey >= 528040)
+                    return "4.8 or later";
+                if (releaseKey >= 461808)
+                    return "4.7.2";
+                if (releaseKey >= 461308)
+                    return "4.7.1";
+                if (releaseKey >= 460798)
+                    return "4.7";
+                if (releaseKey >= 394802)
+                    return "4.6.2";
+                if (releaseKey >= 394254)
+                    return "4.6.1";
+                if (releaseKey >= 393295)
+                    return "4.6";
+                if (releaseKey >= 379893)
+                    return "4.5.2";
+                if (releaseKey >= 378675)
+                    return "4.5.1";
+                if (releaseKey >= 378389)
+                    return "4.5";
+                // This code should never execute. A non-null release key should mean
+                // that 4.5 or later is installed.
+                return "No 4.5 or later version detected";
+            }
+        }
+       
+        private static void GetUpdateHistory()
+        {
+            using (RegistryKey baseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey(@"SOFTWARE\Microsoft\Updates"))
+            {
+                foreach (string baseKeyName in baseKey.GetSubKeyNames())
+                {
+                    if (baseKeyName.Contains(".NET Framework"))
+                    {
+                        using (RegistryKey updateKey = baseKey.OpenSubKey(baseKeyName))
+                        {
+                            Console.WriteLine(baseKeyName);
+                            foreach (string kbKeyName in updateKey.GetSubKeyNames())
+                            {
+                                using (RegistryKey kbKey = updateKey.OpenSubKey(kbKeyName))
+                                {
+                                    Console.WriteLine("  " + kbKeyName);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static Collection<Version> InstalledDotNetVersions()
+        {
+            Collection<Version> versions = new Collection<Version>();
+            RegistryKey NDPKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP");
+            if (NDPKey != null)
+            {
+                string[] subkeys = NDPKey.GetSubKeyNames();
+                foreach (string subkey in subkeys)
+                {
+                    GetDotNetVersion(NDPKey.OpenSubKey(subkey), subkey, versions);
+                    GetDotNetVersion(NDPKey.OpenSubKey(subkey).OpenSubKey("Client"), subkey, versions);
+                    GetDotNetVersion(NDPKey.OpenSubKey(subkey).OpenSubKey("Full"), subkey, versions);
+                }
+            }
+            return versions;
+        }
+
+        private static void GetDotNetVersion(RegistryKey parentKey, string subVersionName, Collection<Version> versions)
+        {
+            if (parentKey != null)
+            {
+                string installed = Convert.ToString(parentKey.GetValue("Install"));
+                if (installed == "1")
+                {
+                    string version = Convert.ToString(parentKey.GetValue("Version"));
+                    if (string.IsNullOrEmpty(version))
+                    {
+                        if (subVersionName.StartsWith("v"))
+                            version = subVersionName.Substring(1);
+                        else
+                            version = subVersionName;
+                    }
+
+                    Version ver = new Version(version);
+
+                    if (!versions.Contains(ver))
+                        versions.Add(ver);
+                }
+            }
+        }
+    }
+}
